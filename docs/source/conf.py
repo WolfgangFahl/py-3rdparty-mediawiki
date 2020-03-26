320508bc0ed7c469da70067a083ebec2067ef96b
Wolfgang Fahl <wf@bitplan.com>
2020-03-26 16:56:11 +0100

    tries fixing doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ copyright = '2020, Wolfgang Fahl'
 author = 'Wolfgang Fahl'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.1a'
 
 
 # -- General configuration ---------------------------------------------------
@@ -49,7 +49,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['modules.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
