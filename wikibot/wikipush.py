1093e8fcb7032ef2b13b3d98ff9adc9bc3a3379e
Wolfgang Fahl <wf@bitplan.com>
2021-05-27 13:56:53 +0200

    tries fixing #66

--- wikibot/wikipush.py
+++ wikibot/wikipush.py
@@ -525,9 +525,9 @@ class WikiPush(object):
             except Exception as ex:
                 self.log("❌:%s" % str(ex) )
 
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 __date__ = '2020-10-31'
-__updated__ = '2021-04-15'
+__updated__ = '2021-05-27'
 DEBUG=False
 
 def mainNuke(argv=None):
