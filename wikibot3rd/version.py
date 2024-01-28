3f98f764bea4d16873a7361905baf43a557a4c9c
Wolfgang Fahl <wf@bitplan.com>
2024-01-28 12:37:38 +0100

    tries fixing yamlable dependency

--- wikibot3rd/version.py
+++ wikibot3rd/version.py
@@ -14,7 +14,7 @@ class Version(object):
     name = "py-3rdparty-mediawiki"
     version = wikibot3rd.__version__
     date = "2020-10-31"
-    updated = "2024-01-24"
+    updated = "2024-01-28"
 
     authors = "Wolfgang Fahl, Tim Holzheim"
 
