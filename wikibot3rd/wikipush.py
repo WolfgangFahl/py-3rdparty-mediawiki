8067a720ad001f776f3a31e2eca562e0988129bb
Wolfgang Fahl <wf@bitplan.com>
2025-02-20 06:43:53 +0100

    fixes #138

--- wikibot3rd/wikipush.py
+++ wikibot3rd/wikipush.py
@@ -998,13 +998,13 @@ def main(argv=None, mode="wikipush"):  # IGNORE:C0111
         program_version,
         program_build_date,
     )
-    program_shortdesc = "wikipush"
+    program_shortdesc = mode
     user_name = "Wolfgang Fahl"
 
     program_license = """%s
 
   Created by %s on %s.
-  Copyright 2020-2024 Wolfgang Fahl. All rights reserved.
+  Copyright 2020-2025 Wolfgang Fahl. All rights reserved.
 
   Licensed under the Apache License 2.0
   http://www.apache.org/licenses/LICENSE-2.0
