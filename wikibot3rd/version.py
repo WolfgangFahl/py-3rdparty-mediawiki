ab77cb81ad242f633362f3919ef82c2ded3d6d83
Wolfgang Fahl <wf@bitplan.com>
2025-03-10 07:20:48 +0100

    refactors to get siteinfo

--- wikibot3rd/version.py
+++ wikibot3rd/version.py
@@ -15,7 +15,7 @@ class Version(object):
     name = "py-3rdparty-mediawiki"
     version = wikibot3rd.__version__
     date = "2020-10-31"
-    updated = "2025-03-08"
+    updated = "2025-03-10"
 
     authors = "Wolfgang Fahl, Tim Holzheim"
 
